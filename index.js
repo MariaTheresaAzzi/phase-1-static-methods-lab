f37407edafef81934e53a16be84fd7c14cb70254
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-09-30 21:17:34 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  static titleize(string) {
+    const arr = string.split(" ");
+    const fixedArr = string
+      .split(" ")
+      .map((e) => {
+        if (arr.indexOf(e) == 0) {
+          return Formatter.capitalize(e);
+        } else if (
+          e == "the" ||
+          e == "a" ||
+          e == "an" ||
+          e == "but" ||
+          e == "of" ||
+          e == "and" ||
+          e == "for" ||
+          e == "at" ||
+          e == "by" ||
+          e == "from"
+        ) {
+          return e;
+        } else {
+          return Formatter.capitalize(e);
+        }
+      })
+      .join(" ");
+    return fixedArr;
+  }
 }
+
